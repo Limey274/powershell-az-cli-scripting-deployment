1609b95db1ff96ee2ff92e8144d7ca142ddb55f4
Limey274 <65303974+Limey274@users.noreply.github.com>
2020-08-11 10:26:23 -0700

    Update provisionResources.ps1

--- provisionResources.ps1
+++ provisionResources.ps1
@@ -1,29 +1,53 @@
 # TODO: set variables
-$studentName = ""
-$rgName = ""
-$vmName = ""
-$vmSize = ""
-$vmImage = ""
-$vmAdminUsername = ""
+$studentName = "Daniel"
+
+$rgName = "${studentName}-events-aadb2c-rg"
+
+$vmName = "${studentName}-events-aadb2c-vm"
+
+$vmSize = "Standard_B2s"
+
+$vmImage = "$(az vm image list --query "[? contains(urn, 'Ubuntu')] | [0].urn")"
+
+$vmAdminUsername = "student"
+$vmAdminPassword='LaunchCode-@zure1'
+
+	#KV INFO
 $kvName = "$studentName-lc0820-ps-kv"
 $kvSecretName = "ConnectionStrings--Default"
 $kvSecretValue = "server=localhost;port=3306;database=coding_events;user=coding_events;password=launchcode"
 
-# TODO: provision RG
+az configure --default location=eastus
 
-# TODO: provision VM
+	# TODO: provision RG
+az group create -n "$rgName"
 
-# TODO: capture the VM systemAssignedIdentity
+az configure --default group=$rgName
 
-# TODO: open vm port 443
 
-# provision KV
+	# TODO: provision VM
+az vm create -n $vmName --size $vmSize --image $vmImage --admin-username $vmAdminUsername --admin-password $vmAdminPassword --authentication-type password --assign-identity | Set-Content vm.json
+az configure --default vm=$vmName
 
+$vm = Get-Content .\vm.json | ConvertFrom-Json
+	
+
+	# TODO: capture the VM systemAssignedIdentity
+$vmId=$vm.identity.systemAssignedIdentity
+
+
+	# TODO: open vm port 443
+az vm open-port --port 443
+
+
+	# provision KV
 az keyvault create -n $kvName --enable-soft-delete false --enabled-for-deployment true
 
-# TODO: create KV secret (database connection string)
+	# TODO: create KV secret (database connection string)
+az keyvault secret set --vault-name $kvName --description 'connection string' --name $kvSecretName --value $kvSecretValue
 
-# TODO: set KV access-policy (using the vm ``systemAssignedIdentity``)
+
+	# TODO: set KV access-policy (using the vm ``systemAssignedIdentity``)
 
 az vm run-command invoke --command-id RunShellScript --scripts @vm-configuration-scripts/1configure-vm.sh
 
@@ -32,4 +56,6 @@ az vm run-command invoke --command-id RunShellScript --scripts @vm-configuration
 az vm run-command invoke --command-id RunShellScript --scripts @deliver-deploy.sh
 
 
-# TODO: print VM public IP address to STDOUT or save it as a file
+	# TODO: print VM public IP address to STDOUT or save it as a file
+echo "VM available at $vmIp"
+
